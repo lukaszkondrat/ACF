a9306d2605811d7a1d2f9f3ea3aa2bd5f900eb37
lukaszkondrat <120600257+lukaszkondrat@users.noreply.github.com>
2024-04-24 16:07:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,35 @@ import react from "@vitejs/plugin-react"
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      'react': 'https://cdn.skypack.dev/react@17',
-      'react-dom': 'https://cdn.skypack.dev/react-dom@17'
-    }
-  },
-  plugins: [react(), eslint()],
-});
+export default {
+    plugins: [
+        importToCDN({
+            modules: [
+                {
+                    name: 'react',
+                    var: 'React',
+                    path: `umd/react.production.min.js`,
+                },
+                {
+                    name: 'react-dom',
+                    var: 'ReactDOM',
+                    path: `umd/react-dom.production.min.js`,
+                },
+            ],
+        }),
+    ],
+}
+
+
+
+
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       'react': 'https://cdn.skypack.dev/react@17',
+//       'react-dom': 'https://cdn.skypack.dev/react-dom@17'
+//     }
+//   },
+//   plugins: [react(), eslint()],
+// });
 
