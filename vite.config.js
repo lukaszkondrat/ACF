d2ae34cb11b3cabc59cac49c2fdc0371e8cd2843
lukaszkondrat <120600257+lukaszkondrat@users.noreply.github.com>
2024-04-24 13:41:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
 });
 
